e7662f323d1d5a07141e4c5ba02a6b70ba1ac521
ameeshadev <140043042+ameeshadev@users.noreply.github.com>
2023-07-20 10:52:37 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,12 @@
+a=20#a is a int
+b=10#b is a int
+c="ameesha"#c is string
+d=True#d is a boolean
+e=False#e is a boolean
+list=["ameesha",1,2]
+tuple=(1,2,3)
+dict={1:3,2:4}
+set={1,2,3}
+print(type(list))
+print(type(tuple))
+print(type(dict))
