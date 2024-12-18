5e8e3a995d1adabd4fc39121b49d9ca8094aa8b1
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-18 20:32:09 +0530

    Create Pass.py

---
+++ Pass.py
@@ -0,0 +1,12 @@
+# using pass statement
+def f1():
+    # defined a function of the f1()
+    print("f1 function ")
+def f2():
+    pass
+# we don't know how to write but it is want to execute the block 
+def f3():
+    print("f3 function ")
+f1()
+f2()
+f3()
