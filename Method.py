d8b1604cdf871fe16648ca18141f53a28e06dd63
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-19 20:56:32 +0530

    Create Method.py

---
+++ Method.py
@@ -0,0 +1,14 @@
+#string method
+#The isalpha() method returns True if all the characters are alphabet letters (a-z).
+a="pythonprogramminglanguagex"
+b=a.isalpha()
+print(b)# it will return true
+# numbers and symbols are not required in string isalpha()
+c="ameesha1075"
+d=c.isalpha()
+print(d)# it will return false
+#isdigit()
+a="Hello3"
+print(a.isdigit())#it will return false 
+b="12345"
+print(b.isdigit())# it will return true because only digits
