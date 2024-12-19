2c6b86e06cd08ef89475ba00b59c03e3c30df35c
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-19 19:24:38 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,11 @@
+#string operation
+a="Hello"
+b="Ameesha"
+c=a+b
+print(c)
+#string slicing
+a="Welcome to python programming"
+print(a[0:29:1])
+print(a[-11::1])#difference is 1
+print(a[-11::-1])
+
