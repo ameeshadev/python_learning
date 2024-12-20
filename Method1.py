1dab619565dcdc1cf3ed51c90e16dac5e66f2446
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-20 18:48:03 +0530

    Create Method1.py

---
+++ Method1.py
@@ -0,0 +1,18 @@
+# isalpha()
+# this mehod using alphabet and numbers
+a="ameeshadev011075"
+print(a.isalnum())
+#isupper()
+# this method every alphabet is string in upper case
+a="ameesha"
+print(a.upper())
+print(a.isupper())
+b="AMEERUNISA"
+# lower method every alphabet is string in lower case
+print(b.islower())
+print(b.lower())
+#little
+x="python programming language "
+print(x.istitle())
+print(x.title())
+# if the string in title case in the string first character in upper case
