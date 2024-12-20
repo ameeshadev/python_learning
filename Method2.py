01b3b1f17a307212f36e102ae3d423702cea4998
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-20 19:06:19 +0530

    Create Method2.py

---
+++ Method2.py
@@ -0,0 +1,10 @@
+a="welcome to python programming language"
+print(a.isidentifier())
+print(a.isprintable())
+#every character is string unable to print then the other characters by using \n it will return escaping character 
+# startwith()
+# if we want any letter stating another word using the method
+print (a.startswith("welcome"))
+#The startswith() method returns True if the string starts with the specified value, otherwise False.
+print(a.endswith("e"))
+#Returns true if the string ends with the specified value otherwis
