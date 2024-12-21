974ac7232750a003664a70ce1108b1bf916fd57a
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-21 21:13:06 +0530

    Create Listmethods.py

---
+++ Listmethods.py
@@ -0,0 +1,37 @@
+#list methods
+numbers=[20,30,40,50,60]
+numbers.insert(2,100)
+print(numbers)
+#this is append method
+numbers.append("ameesha")
+print(numbers)
+#this is extended method 
+num=[22,33,45,67,89,40]
+s="ameesha"
+num.extend(s)
+print(num)
+#this is remove method
+a=[23,40,55,67]
+a.remove(40)
+print(a)
+#pop method it is calculating index value
+a=[23,45]
+a.pop(0)
+print(a)
+#clear method this method is created an empty list
+my_list=["ameesha"]
+my_list.clear()
+print(my_list)
+my_list.copy()
+print(my_list)
+#reverse and count methods
+j=[22,33,44,55]
+j.reverse()
+print(j)
+i=[23,34,45,56]
+d=i.count(23)
+print(d)
+#index and sort method
+s=[33,55,77,99]
+k=s.index(55)
+print(k
