5a57ef49085fa94ef7306e6c78bdc9b3b96a9c3b
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-21 21:41:58 +0530

    Create tuple method.py

---
+++ tuple method.py
@@ -0,0 +1,24 @@
+#nested tuple
+t=(10,20,30,(40,50,60),70)
+a=t[3][0]
+print(a)
+t1=(22,33,44,[44,77,99],43,23)
+d=t1[3][0]
+print(d)
+#we can use any negative values or positive values
+#tuple methods
+a=(10,3378,69,50,69)
+b=a.index(3378)
+print(a)
+#count method
+c=a.count(69)
+print (c)
+#length method
+d=len(a)
+print(d)
+#minimum method
+i=min(a)
+print(i)
+#maximum method
+x=max(a)
+print(x
