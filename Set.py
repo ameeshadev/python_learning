8796ebee53eb79e06a2337eabfc77d110620cee7
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-22 06:29:09 +0530

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,12 @@
+#sets
+#it can store any number and any type of value is immutable
+s={1,2,3,4,5}
+#it is valid statement
+s1={1,2,(3,4,5),6,8}
+#this is also a valid statement but not using the lists and sets inside the set
+print(s)
+print(s1)
+#we also use string
+a="welcome to python"
+b={ch.upper() for ch in a}
+print(b)
