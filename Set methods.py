0f467384ffa0545225663d1a90d55ceacd6b9e6a
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-22 06:53:06 +0530

    Create Set methods.py

---
+++ Set methods.py
@@ -0,0 +1,21 @@
+#sets methods
+a={1,2,3,4,5,6,7,8,9,10}
+a.add(20)
+print(a)
+#update method also same as add method
+s={22,33,4,55,66}
+s.remove(33)
+print(s)
+#discard method also same as remove method 
+s.discard(33)
+print(s)
+#the pop method we can delete any random number 
+s.pop()
+print (s)
+#clear method we can clear the elements are the empty
+s.clear()
+print(s)
+#copy method
+a={"ameesha"}
+b=a.copy()
+print(b)
