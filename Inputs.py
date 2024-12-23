069c65c546a7cb9ce6c05ab6c3adb8215a3c828c
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-23 08:33:41 +0530

    Create Inputs.py

---
+++ Inputs.py
@@ -0,0 +1,19 @@
+#reading a input function
+num=input("enter a string: ")
+print(num)
+print (type(num))
+#now the string is converted to a int or float
+numbers=int(input("Enter a number: "))
+print(numbers)
+print(type(numbers))
+#floating point
+a=float(input("Enter a floating point number: "))
+print(a)
+print(type(a))
+#character input
+char=(input("Enter a character: "))
+print(char)
+print(type(char))
+# this are the reading the input function 
+#we can use type of the datatypes we cam mention the
+#type(variable)
