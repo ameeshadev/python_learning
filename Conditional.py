de3f9d74b042d045b86f20c3539ab24eb59e8c74
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-25 13:24:44 +0530

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,34 @@
+#conditional statements
+#if statement
+a=10
+b=40
+if (a<b):
+    print("right")
+#else statement
+else:
+    print ("wrong ")
+#if-else statement
+a=22
+b=33
+if (a==b):
+    print ("a is equal to b ")
+else:
+    print ("ais not equal to b")
+#elif statement
+a=0
+if (a==1):
+    print ("yes")
+elif (a==0):
+    print ("good")
+else:
+    print("no")
+#nested statement
+a=20
+if (a==20):
+    if(a>30):
+        print("welcome")
+    else:
+        print ("no welcome ")
+        
+else:
+    print ("welcome to condition statements")
