2d0dcfdca8078b69c57a5aeef2fef88865ff48f8
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-29 11:48:44 +0530

    Create Flowchart.py

---
+++ Flowchart.py
@@ -0,0 +1,21 @@
+#flowchart
+#terminal
+print ("start")
+#input/output
+my_num=int(input("enter a number:"))
+#process
+if my_num>0:
+    print ("number is greater")
+else:
+    print("number is smaller")
+#decision
+if my_num==0:
+    print ("true")
+elif my_num<15:
+    print ("right")
+elif my_num>20:
+    print ("wrong ")
+else:
+    print ("false")
+#terminal
+print ("end")
