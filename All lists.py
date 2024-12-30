3eb74798140f47c026fda8b4935be9fa1a8b10a9
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-30 21:34:46 +0530

    Create All lists.py

---
+++ All lists.py
@@ -0,0 +1,24 @@
+#python lists
+#using lists[]
+a=[22,33,56,78,90]
+print(a)
+#using list() constructor
+a=((10,40,60,80,100))
+print (a)
+#repeated elements
+a=[40]*4
+print (a)
+#accessing list
+a=[22,34,56,78,90]
+print (a[1])
+#append
+a.append(50)
+a.insert(0,5)
+#updating
+a[2]=40
+#removing
+a=[34,55,77,99]
+a.remove(34)
+a.pop(1)
+del a[0]
+print (a)
