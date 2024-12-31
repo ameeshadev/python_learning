edd0cb27432b213403394365652085ea0dfa1adf
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-31 17:38:10 +0530

    Create Tuple operation 1.py

---
+++ Tuple operation 1.py
@@ -0,0 +1,16 @@
+# Code to create a tuple with repetition
+t = ('python',)*3
+print(t)
+# code to test slicing
+t = (0 ,1, 2, 3)
+
+print(t[1:])
+print(t[::-1])
+print(t[2:4])
+# Code for deleting a tuple
+t = ( 0, 1)
+del t
+print(t)
+# Code for printing the length of a tuple
+t = ('python', 'geek')
+print(len(t))
