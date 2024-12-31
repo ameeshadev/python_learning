fdc70e9f97e4f69f3b0aefcfedd7d31b8a68f775
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-31 17:32:59 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,19 @@
+t = (1, 2, 3, 4, 5)
+
+# tuples are indexed
+print(t[1])
+print(t[4])
+
+# tuples contain duplicate elements
+t = (1, 2, 3, 4, 2, 3)
+print(t)
+
+# updating an element
+t[1] = 100
+print(t)
+#accessing tuples
+t = (10, 5, 20)
+
+print("Value in t[0] = ", t[0])
+print("Value in t[1] = ", t[1])
+print("Value in t[2] = ", t[2])
