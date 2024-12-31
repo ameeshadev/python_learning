a97031681f0d0b1962bcf9816e6911096ce3d4a8
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2024-12-31 17:35:32 +0530

    Create Tuple operation.py

---
+++ Tuple operation.py
@@ -0,0 +1,19 @@
+# Define a tuple
+t = (1, 2, 3, 4, 5)
+
+# Traverse through each item in the tuple
+for x in t:
+    print(x, end=" ")
+    
+# Code for concatenating 2 tuples
+t1 = (0, 1, 2, 3)
+t2 = ('python', 'geek')
+
+# Concatenating above two
+print(t1 + t2)
+# Code for creating nested tuples
+t1 = (0, 1, 2, 3)
+t2 = ('python', 'geek')
+
+t3 = (t1, t2)
+print(t3)
