d7659f5a423973101fb746941a2f919f92200631
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-01-01 20:45:27 +0530

    Create Super inheritance.py

---
+++ Super inheritance.py
@@ -0,0 +1,16 @@
+# Parent Class: Person
+class Person:
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+# Child Class: Employee
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        super().__init__(name, idnumber)  # Using super() to call Person's __init__()
+        self.salary = salary
+        self.post = post
