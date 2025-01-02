955bb26f5d20042f40413501099d81af2d71f2d9
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-01-02 18:46:52 +0530

    Create Dates and time.py

---
+++ Dates and time.py
@@ -0,0 +1,16 @@
+#dates in python
+# we can import named datetime
+#creating current date
+import datetime
+x=datetime.datetime.now()
+print(x)
+#return the year and name of weekdays
+import datetime
+a=datetime.datetime.now()
+print(a.year)
+print(a.strftime("%A"))
+#strftime it is string format it is indicated weekday
+#creating datetime() method or constructor
+import datetime
+a=datetime.datetime(2025,1,2)
+print(x)
