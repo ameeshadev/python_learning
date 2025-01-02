21921f4a79d99be9c3f253f2a51affb6493dba25
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2025-01-02 19:11:06 +0530

    Create Math function.py

---
+++ Math function.py
@@ -0,0 +1,18 @@
+#math functions
+#we can use built in functions min(),max(),abs(),pow(),sqrt()
+#minimum function
+x=min(10,40,60)
+y=max(10,44,78)
+print(x)
+print(y)
+#absolute it will gives only positive number
+a=abs(-34.66)
+print (a)
+#power returns the value of x to the power of y
+a=pow(3,4)
+print(a)
+#math.sqrt()
+#methid for example return the square root of a number
+import math
+x=math.sqrt(5678)
+print(x)
